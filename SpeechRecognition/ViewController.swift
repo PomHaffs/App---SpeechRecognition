d4431ec3f3c5f9cad365d3515a552cd604f4f191
PomHaffs <tomashaffenden@icloud.com>
2016-11-23 12:50:00 +1100

    Added all logic and transciption

--- SpeechRecognition/ViewController.swift
+++ SpeechRecognition/ViewController.swift
@@ -11,25 +11,84 @@ import UIKit
 import Speech
 import AVFoundation
 
-class ViewController: UIViewController {
+//Added AvAudioPlayerDelegate
+class ViewController: UIViewController, AVAudioPlayerDelegate {
 
     @IBOutlet weak var ActivitySpinner: UIActivityIndicatorView!
     
     @IBOutlet weak var TranscriptionTextField: UITextView!
     
+    var audioPlayer: AVAudioPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
        ActivitySpinner.isHidden = true
     }
 
+//This searches for the player and stops it
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        player.stop()
+        ActivitySpinner.stopAnimating()
+        ActivitySpinner.isHidden = true
+    }
+    
     func requestSpeechAuth() {
         SFSpeechRecognizer.requestAuthorization { authStatus in
             if authStatus == SFSpeechRecognizerAuthorizationStatus.authorized {
-                
+                if let path = Bundle.main.url(forResource: "test", withExtension: "m4a") {
+//This DO block players the audio
+                    do {
+                        let sound = try AVAudioPlayer(contentsOf: path)
+                        self.audioPlayer = sound
+                        self.audioPlayer.delegate = self
+                        sound.play()
+                    } catch {
+                        print("Error!!!")
+                    }
+//This is starting the transcription process
+                    let recognizer = SFSpeechRecognizer()
+                    let request = SFSpeechURLRecognitionRequest(url: path)
+                    recognizer?.recognitionTask(with: request) { (result, error) in
+                        if let error = error {
+                        print("There was an error \(error)")
+                        } else {
+                            self.TranscriptionTextField.text = result?.bestTranscription.formattedString
+                        }
+                    }
+                }
             }
         }
-    }
 
+    }
+//On pushing button we have started the animation for the spinner
+
+    @IBAction func playButtonPressed(_ sender: Any) {
+        print("got to this")
+        ActivitySpinner.isHidden = false
+        ActivitySpinner.startAnimating()
+        requestSpeechAuth()
+    }
+    
+    
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
