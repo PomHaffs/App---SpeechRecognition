18929dd2709cb76ce7065ab6eccc98d34e9fecc2
PomHaffs <tomashaffenden@icloud.com>
2016-11-23 11:27:11 +1100

    First commit

---
+++ SpeechRecognition/CircleButton.swift
@@ -0,0 +1,21 @@
+//
+//  CircleButton.swift
+//  SpeechRecognition
+//
+//  Created by Tomas-William Haffenden on 23/11/16.
+//  Copyright © 2016 PomHaffs. All rights reserved.
+//
+
+import UIKit
+
+class CircleButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
